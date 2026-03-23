b785a0302f6e12bf83db31a407212a66a3c28f96
Lucas Brasileiro <lucasbrasileiro_789@hotmail.com>
2026-03-23 16:18:18 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Signin from './pages/Signin'
 import Register from './pages/Register'
 import Dashboard from './pages/Dashboard'
+import ForgotPassword from './pages/ForgotPassword'
+import ValidateCode from './pages/ValidateCode'
+import ResetPassword from './pages/ResetPassword'
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
         <Route path="/" element={<Signin />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/validate-code" element={<ValidateCode />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
       </Routes>
     </BrowserRouter>
   )
